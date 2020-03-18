c1baaa085b68ae47033b57ae36fc8a82e0629559
Rafael Burgueño <rafaelburg@gmail.com>
2020-03-17 22:45:50 -0300

    realice la tarea de borrar audios

--- app/src/main/java/com/example/auraudiorecorder/RecordingAdapter.java
+++ app/src/main/java/com/example/auraudiorecorder/RecordingAdapter.java
@@ -18,6 +18,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
+import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -104,8 +105,14 @@ public class RecordingAdapter extends RecyclerView.Adapter<RecordingAdapter.View
 
                                 break;
                             case R.id.actionEliminar:
-                                lanzarConfirmarBorrado(null);
-                                return true;
+                                String archivo = recordingArrayList.get(position).getFileName();
+                                lanzarConfirmarBorrado(archivo, holder, position);
+
+                                //
+
+
+                                //return true;
+                                break;
                             case R.id.actionAdjuntarNota:
                                 //manejar el clic sobre Adjuntar nota
                                 break;
@@ -195,20 +202,44 @@ public class RecordingAdapter extends RecyclerView.Adapter<RecordingAdapter.View
         holder.manageSeekBar(holder);
     }
 
-    private void lanzarConfirmarBorrado(View view) {
+    private void lanzarConfirmarBorrado(final String archivo, final ViewHolder miViewHolder, final int position) {
         final TextView alerta = new TextView(context);
         new AlertDialog.Builder(context)
                 .setTitle(R.string.borrar_grabacion)
-                .setMessage(R.string.desea_borrar)
+                .setMessage(R.string.desea_borrar + " -> " + archivo)
                 .setView(alerta)
                 .setPositiveButton(R.string.borrar, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
+                        File root = android.os.Environment.getExternalStorageDirectory();
+                        String path = root.getAbsolutePath() + "/AUR/Audios/";
+
+                        File audio = new File(path + archivo);
                         //llamar accion de borrar
+                        audio.delete();
+
+                        //oculta la vista del audio en la RecordingListActivity o reinicia la activity
+                        miViewHolder.imageViewPlay.setVisibility(View.GONE);
+                        miViewHolder.textViewName.setVisibility(View.GONE);
+                        miViewHolder.botonMore.setVisibility(View.GONE);
+
+                        miViewHolder.linea.setVisibility(View.GONE);
+                        miViewHolder.itemGrabacion.setPadding(0,0,0,0);
+                        miViewHolder.itemGrabacion.setPaddingRelative(0,0,0,0);
+
+                        miViewHolder.itemGrabacion.setVisibility(View.GONE);
+                        //notifyItemRangeRemoved(position+1, position);
+
                         Toast.makeText(context, R.string.se_eligio_borrar, Toast.LENGTH_SHORT).show();
                     }
                 })
-                .setNegativeButton(R.string.cancelar, null)
+                .setNegativeButton(R.string.cancelar, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //llamar accion de NO borrar
+                        Toast.makeText(context, "se eligio NO BORRAR", Toast.LENGTH_SHORT).show();
+                    }
+                })
                 .show();
     }
 
@@ -233,10 +264,16 @@ public class RecordingAdapter extends RecyclerView.Adapter<RecordingAdapter.View
         private Handler mHandler = new Handler();
         ViewHolder holder;
 
+        RelativeLayout itemGrabacion;
+        View linea;
+
 
 
         public ViewHolder(View itemView) {
             super(itemView);
+
+            itemGrabacion = itemView.findViewById(R.id.itemLayout);
+            linea = itemView.findViewById(R.id.linea);
 
             imageViewPlay = itemView.findViewById(R.id.imageViewPlay);
             seekBar = itemView.findViewById(R.id.seekBar);
